fdc910180e456a33a034ffaad67e22f8fdfffd7a
tinoco123 <tinocojona@outlook.com>
2021-01-12 15:00:19 -0600

    Empezamos a dibujar con Canvas

--- scripts/codigo.js
+++ scripts/codigo.js
@@ -10,6 +10,33 @@ function dibujar_linea(color,x1, y1, x2, y2)
     lienzo.stroke();
     lienzo.closePath;
 }
+var limit = 30
+var l = 0
+var aumento = 0
 
-dibujar_linea("black", 100,100, 200, 200);
-dibujar_linea("red", 125,125, 225, 225);
+
+while (l < limit)
+{
+    dibujar_linea("black", 0, aumento, aumento, 300);
+    dibujar_linea("blue", 300, aumento, aumento, 0);
+    
+    aumento += 10;
+    l += 1;
+    console.log(l, aumento);
+}
+l = 0;
+aumento = 0
+var aumentoy = 300;
+while (l < limit)
+{
+    dibujar_linea("yellow", 300, aumento, aumentoy, 300);
+    dibujar_linea("pink", 0, aumento, aumentoy, 0);
+    
+    aumento += 10;
+    aumentoy -= 10;
+    l += 1;
+}
+
+
+
+
