a7ad0e9453c79a1dcf36755b339c9e44bbeb2eb2
tinoco123 <tinocojona@outlook.com>
2021-01-15 01:06:41 -0600

    Nueva funcionalidad

--- scripts/codigo.js
+++ scripts/codigo.js
@@ -1,5 +1,9 @@
 var can = document.getElementById("dibujo");
 var lienzo = can.getContext("2d");
+var num_lineas = document.getElementById("id_lineas")
+var boton_lineas = document.getElementById("id_enviar_lineas")
+
+
 
 function dibujar_linea(color,x1, y1, x2, y2)
 {
@@ -10,31 +14,38 @@ function dibujar_linea(color,x1, y1, x2, y2)
     lienzo.stroke();
     lienzo.closePath;
 }
-var limit = 30
-var l = 0
-var aumento = 0
 
 
-while (l < limit)
-{
-    dibujar_linea("black", 0, aumento, aumento, 300);
-    dibujar_linea("blue", 300, aumento, aumento, 0);
+boton_lineas.addEventListener("click", dibujo_click);
+
+function dibujo_click(){
+    var lineas = parseInt(num_lineas.value);
     
-    aumento += 10;
-    l += 1;
-    console.log(l, aumento);
-}
-l = 0;
-aumento = 0
-var aumentoy = 300;
-while (l < limit)
-{
-    dibujar_linea("yellow", 300, aumento, aumentoy, 300);
-    dibujar_linea("pink", 0, aumento, aumentoy, 0);
-    
-    aumento += 10;
-    aumentoy -= 10;
-    l += 1;
+    var limit = lineas
+    espacio = can.height / limit;
+    var l = 0
+    var aumento = 0
+    while (l < limit)
+    {
+        dibujar_linea("black", 0, aumento, aumento, can.height - 1);
+        dibujar_linea("blue", can.width - 1, aumento, aumento, 0);
+        
+        aumento += espacio;
+        l += 1;
+        
+    }
+    l = 0;
+    aumento = 0
+    var aumentoy = can.height;
+    while (l < limit)
+    {
+        dibujar_linea("yellow", can.width - 1, aumento, aumentoy, can.height - 1);
+        dibujar_linea("pink", 0, aumento, aumentoy, 0);
+        
+        aumento += espacio;
+        aumentoy -= espacio;
+        l += 1;
+    }
 }
 
 
